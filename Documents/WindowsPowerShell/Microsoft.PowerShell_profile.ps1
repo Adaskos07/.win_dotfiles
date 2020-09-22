ea4560de224c75b3a0113c59f8111dfba7e68eae
Adam Sulak <a.sulak@protonmail.com>
2020-09-22 15:33:22 +0200

    change function name in profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 $VIMRC = "$HOME\AppData\Local\nvim\init.vim"
 $TERMRC = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
-function config_repo_do($arg1, $arg2, $arg3, $arg4) {
+function Change-Config($arg1, $arg2, $arg3, $arg4) {
 	git --git-dir=$HOME\.win_dotfiles --work-tree=$HOME $arg1 $arg2 $arg3 $arg4
 }
-New-Alias -Name config -Value config_repo_do
+New-Alias -Name config -Value Change-Config
 
 New-Alias -Name v -Value nvim
 New-Alias -Name g -Value git
