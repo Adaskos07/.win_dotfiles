f504960e381ce23b162f0ac3193ec4c76ad2ef33
Adam Sulak <a.sulak@protonmail.com>
2020-09-22 12:49:21 +0200

    add powershell profile

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+$VIMRC = "$HOME\AppData\Local\nvim\init.vim"
+$TERMRC = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+function config_repo_do($arg1, $arg2, $arg3, $arg4) {
+	git --git-dir=C:\Users\"Adam Sulak"\.win_dotfiles --work-tree=C:\Users\"Adam Sulak" $arg1 $arg2 $arg3 $arg4
+}
+New-Alias -Name config -Value config_repo_do
+
+New-Alias -Name v -Value nvim
+New-Alias -Name g -Value git
+
+Get-Date
