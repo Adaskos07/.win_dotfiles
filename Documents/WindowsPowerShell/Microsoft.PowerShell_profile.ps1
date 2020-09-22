498af261573dd1b9161c7012d68bfa6fe718928a
Adam Sulak <a.sulak@protonmail.com>
2020-09-22 12:52:27 +0200

    update powershell config

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@ $VIMRC = "$HOME\AppData\Local\nvim\init.vim"
 $TERMRC = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 function config_repo_do($arg1, $arg2, $arg3, $arg4) {
-	git --git-dir=C:\Users\"Adam Sulak"\.win_dotfiles --work-tree=C:\Users\"Adam Sulak" $arg1 $arg2 $arg3 $arg4
+	git --git-dir=$HOME\.win_dotfiles --work-tree=$HOME $arg1 $arg2 $arg3 $arg4
 }
 New-Alias -Name config -Value config_repo_do
 
