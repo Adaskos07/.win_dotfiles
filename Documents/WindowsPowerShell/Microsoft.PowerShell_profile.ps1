a0e2a778972e216a9f46dda56b63f55fcdf47e6d
Adam Sulak <a.sulak@protonmail.com>
2020-09-23 09:51:37 +0200

    change wars in profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-$VIMRC = "$HOME\AppData\Local\nvim\init.vim"
-$TERMRC = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+$VRC = "$HOME\AppData\Local\nvim\init.vim"
+$TRC = "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 function Change-Config($arg1, $arg2, $arg3, $arg4) {
 	git --git-dir=$HOME\.win_dotfiles --work-tree=$HOME $arg1 $arg2 $arg3 $arg4
